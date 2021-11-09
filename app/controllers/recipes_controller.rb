88b4bcbce424795ddb06b43e22b97fba1353aa84
Swislok-Dev <cmarker4@gmail.com>
2021-11-09 14:37:54 -0600

    add CRUD methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,47 @@
 class RecipesController < ApplicationController
+
+  def index
+    recipes = Recipe.all
+    if recipes.present?
+      render json: recipes
+    else 
+      render json: { message: "No recipes found" }
+    end
+  end
+
+  def create
+    recipe = Recipe.create(recipe_params)
+    render json: recipe
+  end
+
+  def show
+    recipe = Recipe.find_by(id: params[:id])
+    if recipe
+      render json: recipe
+    else
+      render json: { message: "No recipe found" }
+    end
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+    recipe.update(recipe_params)
+    render json:
+  end
+
+  def destroy
+    @recipe.destroy
+    render json:
+  end
+
+  private
+
+  def get_recipe
+    @recipe = Recipe.find_by(id: params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title)
+  end
+
 end
