2a5a5730f19e38e986b50ef8d31b5c335daa5383
Swislok-Dev <cmarker4@gmail.com>
2021-11-26 18:46:47 -0600

    use create method for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,12 @@ class UsersController < ApplicationController
   end
 
   def create 
-    user = User.find_or_create_by(user_params)
-    render json: user
+    user = User.create(user_params)
+    if user.save
+      render json: user
+    else
+      render json: { message: "User was not created" }
+    end
   end
 
   def show 
