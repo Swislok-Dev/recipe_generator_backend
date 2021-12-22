1af6bbf714885209d8f27871f1e200904e5c8709
Swislok-Dev <cmarker4@gmail.com>
2021-12-22 01:07:10 -0600

    correct double @

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   helper_method :login!, :logged_in?, :current_user,
     :authorized_user?, :logout!, :set_user
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::API
   # end
 
   def login!
-    session[:user_id] = @@user.id
+    session[:user_id] = @user.id
   end
   
   def logged_in?
