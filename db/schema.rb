4072a0265a007c3c9bb3d35fbf65938223dc2c42
Swislok-Dev <cmarker4@gmail.com>
2021-12-29 20:41:30 -0600

    remove image column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_142049) do
+ActiveRecord::Schema.define(version: 2021_12_30_024026) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2021_12_16_142049) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "ingredients"
     t.string "instructions"
-    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
