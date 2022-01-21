f58e62aee095767fe73bc5dc733be4ae3c069df7
Swislok-Dev <cmarker4@gmail.com>
2022-01-20 22:37:30 -0600

    validate uniqueness of recipe title

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :reviews
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :ingredients, presence: true
   validates :instructions, presence: true
 end
