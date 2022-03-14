87bebc9bb6a459ef627fbe989d706acdb1ef2c41
Swislok-Dev <cmarker4@gmail.com>
2022-03-14 12:40:36 -0500

    Does not allow only whitespace to be entered

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :recipe
 
   validates :rating, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 1 }
-  
+  validates :content, presence: true
   scope :index_of_reviews, ->(recipe_id){ where("reviews.recipe_id = ?", recipe_id) }
 end
