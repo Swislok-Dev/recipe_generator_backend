0e71e3ca7a36c96a29922f2a8b543ed8ca803f02
Swislok-Dev <cmarker4@gmail.com>
2022-05-15 16:24:50 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,37 +9,37 @@
 
 # swislok = User.create(username: "Swislok", password: "123")
 
-recipe_1 = swislok.recipes.create(
-  name: "The Classic BLT",
+# recipe_1 = swislok.recipes.create(
+#   name: "The Classic BLT",
+#   ingredients: "4 slices of bacon, 2 leaves of lettuce, 2 slices of tomato, 2 slices of toasted bread, 1 tablespoon of mayonnaise", 
+#   instructions: "Cook the bacon in a skillet over medium-high heat until browned. Arrange the cooked bacon, lettuce, and tomato slices on one slice of bread. Spread one side of remaining bread slice with the mayo. Bring the two pieces together to make a sandwich."
+# )
+
+one_recipe = Recipe.create(
+  name: "The Classic BLT", 
   ingredients: "4 slices of bacon, 2 leaves of lettuce, 2 slices of tomato, 2 slices of toasted bread, 1 tablespoon of mayonnaise", 
-  instructions: "Cook the bacon in a skillet over medium-high heat until browned. Arrange the cooked bacon, lettuce, and tomato slices on one slice of bread. Spread one side of remaining bread slice with the mayo. Bring the two pieces together to make a sandwich."
+  instructions: "Cook the bacon in a skillet over medium-high heat until browned. Arrange the cooked bacon, lettuce, and tomato slices on one slice of bread. Spread one side of remaining bread slice with the mayo. Bring the two pieces together to make a sandwich.",
 )
 
-# one_recipe = Recipe.create(
-#   name: "The Classic BLT", 
-#   ingredients: "4 slices of bacon, 2 leaves of lettuce, 2 slices of tomato, 2 slices of toasted bread, 1 tablespoon of mayonnaise", 
-#   instructions: "Cook the bacon in a skillet over medium-high heat until browned. Arrange the cooked bacon, lettuce, and tomato slices on one slice of bread. Spread one side of remaining bread slice with the mayo. Bring the two pieces together to make a sandwich.",
-# )
+one_recipe.reviews.create(
+  rating: 3,
+  content: "This looks pretty basic."
+)
 
-# one_recipe.reviews.create(
-#   rating: 3,
-#   content: "This looks pretty basic."
-# )
+one_recipe.reviews.create(
+  rating: 5,
+  content: "I think this actually looks pretty good"
+)
 
-# one_recipe.reviews.create(
-#   rating: 5,
-#   content: "I think this actually looks pretty good"
-# )
+two_recipe = Recipe.create(
+  name: "The Classic BLT 2", 
+  ingredients: "4 slices of bacon, 2 leaves of lettuce, 2 slices of tomato, 2 slices of toasted bread, 1 tablespoon of mayonnaise", 
+  instructions: "Cook the bacon in a skillet over medium-igh heat until browned. Arrange the cooked bacon, lettuce, and tomato slices on one slice of bread. Spread one side of remaining bread slice with the mayo. Bring the two pieces together to make a sandwich.",
+)
 
-# two_recipe = Recipe.create(
-#   name: "The Classic BLT 2", 
-#   ingredients: "4 slices of bacon, 2 leaves of lettuce, 2 slices of tomato, 2 slices of toasted bread, 1 tablespoon of mayonnaise", 
-#   instructions: "Cook the bacon in a skillet over medium-igh heat until browned. Arrange the cooked bacon, lettuce, and tomato slices on one slice of bread. Spread one side of remaining bread slice with the mayo. Bring the two pieces together to make a sandwich.",
-# )
-
-# two_recipe.reviews.create(
-#   rating: 5,
-#   content: "This look pretty great!"
-# )
+two_recipe.reviews.create(
+  rating: 5,
+  content: "This look pretty great!"
+)
 
 puts "SEEDED"
